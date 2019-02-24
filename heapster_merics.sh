c9ea924e568e87ff0ee062dbb60dab136779b1a5
rajmechi <39564006+rajmechi@users.noreply.github.com>
2019-02-24 13:16:03 -0500

    Delete heapster_merics.sh

--- heapster_merics.sh
+++
@@ -1,7 +0,0 @@
-
-APIPROXY=${MASTERURL}/api/v1/proxy/namespaces/openshift-infra/services
-HEAPSTERAPI=https:heapster:/api/v1/model
-TOKEN=$(oc whoami -t)
-curl -k -H "Authorization: Bearer $TOKEN" -X GET $APIPROXY/$HEAPSTER/$NODE/metrics/memory/working_set?start=$START
-
-https://github.com/kubernetes-retired/heapster/blob/master/docs/storage-schema.md
