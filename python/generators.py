bc0d4cd97072009011517ef46237ddd5f18e047c
rajmechi <39564006+rajmechi@users.noreply.github.com>
2019-09-14 09:55:02 -0400

    Create generators.py

---
+++ python/generators.py
@@ -0,0 +1,36 @@
+
+def square_members(nums):
+    result = []
+    for i in nums:
+        result.append(i*i)
+    return result
+
+my_nums = square_members([1,2,3,4,5,6])
+
+print(my_nums)
+
+
+
+def square_members(nums):
+    for i in nums:
+        yield (i*i)
+
+
+my_nums = square_members([1,2,3,4,5,6])
+
+print(next(my_nums))
+print(next(my_nums))
+print(next(my_nums))
+print(next(my_nums))
+print(next(my_nums))
+
+
+def square_members(nums):
+    for i in nums:
+        yield (i*i)
+        
+my_nums = square_members([1,2,3,4,5,6])
+#above is generator
+
+for num in my_nums:
+    print(num)
